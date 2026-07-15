6844c085c6953d776cfe088635d965b2d08147ab
Ash-exwo <aswathygr61@gmail.com>
2026-07-15 22:56:41 +0530

    SignUp UI Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignUp from "./components/SignUp"
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
